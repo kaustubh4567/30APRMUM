5ecdb4adb4a1b0b676e3f2bdb06edbe5bb8ca05d
Kaustubh <Kaustubh@LAPTOP-NFSLI0GM>
2022-09-11 10:36:38 +0530

    HI KK Create 2 new class Whats up

---
+++ src/sample1GITHUB/Demo1GITHUB.java
@@ -0,0 +1,10 @@
+package sample1GITHUB;
+
+public class Demo1GITHUB
+{
+	public static void main(String[]args)
+	{
+		System.out.println("KK");
+	}
+
+}
---
+++ src/sample1GITHUB/Demo2GITHUB.java
@@ -0,0 +1,9 @@
+package sample1GITHUB;
+
+public class Demo2GITHUB 
+{
+public static void main(String[]args)
+{
+	System.out.println("Chaitanya");
+}
+}
